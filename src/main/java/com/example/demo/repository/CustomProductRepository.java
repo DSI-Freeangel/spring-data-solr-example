4cb6e3ff99188b26c2b794665b7cd0e9456ccc3d
illia.moskaliuk <ilua.moskaluk@gmail.com>
2018-12-24 18:10:06 +0200

    Remove unused imports

--- src/main/java/com/example/demo/repository/CustomProductRepository.java
+++ src/main/java/com/example/demo/repository/CustomProductRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Product;
 import io.reactivex.Flowable;
 
-import java.util.List;
-
 public interface CustomProductRepository {
     Flowable<Product> performStreamSearch();
 }

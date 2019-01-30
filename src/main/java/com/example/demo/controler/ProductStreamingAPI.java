b13b174bd11fd601373a12ff67faa9c5c8fae4ac
illia.moskaliuk <ilua.moskaluk@gmail.com>
2019-01-30 13:06:03 +0200

    Add swagger.

--- src/main/java/com/example/demo/controler/ProductStreamingAPI.java
+++ src/main/java/com/example/demo/controler/ProductStreamingAPI.java
@@ -4,6 +4,8 @@ import com.example.demo.Constants;
 import com.example.demo.dto.IProduct;
 import com.example.demo.service.ProductService;
 import io.reactivex.Flowable;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -17,11 +19,18 @@ public class ProductStreamingAPI {
     private final ProductService service;
 
     @RequestMapping(path = "/findAllUsingSolrTemplate", method = RequestMethod.GET)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Failure", response = IProduct.class, responseContainer = "List")
+    })
     public Flowable<IProduct> findAllUsingSolrTemplate(@PageableDefault(page = 0, size = Constants.DEFAULT_PAGE_SIZE) Pageable pageable) {
         return service.findUsingSolrTemplate(pageable);
     }
 
     @RequestMapping(path = "/findAllUsingStreamFactory", method = RequestMethod.GET)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Failure", response = IProduct.class, responseContainer = "List")
+    })
+
     public Flowable<IProduct> findAllUsingStreamFactory(@PageableDefault(page = 0, size = Constants.DEFAULT_PAGE_SIZE) Pageable pageable) {
         return service.findUsingStreamFactory(pageable);
     }
